e7284c9b29161270ce5e52ba522d9d0fce3fc56e
MakXulu <m.aamir.it@gmail.com>
2020-06-06 07:30:20 +0800

    Initial comments

---
+++ src/main/java/uthm/javabook/App.java
@@ -0,0 +1,13 @@
+package uthm.javabook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/uthm/javabook/textAnalaysis/SentenceDetectorClass.java
@@ -0,0 +1,10 @@
+package uthm.javabook.textAnalaysis;
+
+public class SentenceDetectorClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
