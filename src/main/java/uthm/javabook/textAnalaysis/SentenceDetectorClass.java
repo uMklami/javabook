573ed98c5e53b58a82a99e9e4690ef64677224a4
MakXulu <m.aamir.it@gmail.com>
2020-06-06 09:36:40 +0800

    Code updated for text analysis

--- src/main/java/uthm/javabook/textAnalaysis/SentenceDetectorClass.java
+++ src/main/java/uthm/javabook/textAnalaysis/SentenceDetectorClass.java
@@ -1,10 +1,44 @@
 package uthm.javabook.textAnalaysis;
 
+/*
+ * Author : Aamir Khan
+ * Faculty of Computer Science and Information Technology
+ * University Tun Hussein Onn Malaysia
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+
 public class SentenceDetectorClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String rawText = "Hi everyone? Welcome to the Java Book Programs from FSKTM UTHM. "
+				+ "This is a test code for sentence detector. hope you will find it easy to understand.";
+
+		try {
+			// Load model for sentence detector from your models location.
+			// In my case is "C:/apache-opennlp-1.9.2/models/"
+			InputStream inputModel = new FileInputStream("C:/apache-opennlp-1.9.2/models/da-sent.bin");
+
+			SentenceModel sentenceDetectModel = new SentenceModel(inputModel);
+
+			// Instantiate the SentenceDetectorME class for loading its methods
+			SentenceDetectorME sentenceDetector = new SentenceDetectorME(sentenceDetectModel);
+
+			// List of detected sentences
+			String sentencesList[] = sentenceDetector.sentDetect(rawText);
+
+			// Print all detected sentences in the list.
+			for (String sentence : sentencesList)
+				System.out.println(sentence);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
